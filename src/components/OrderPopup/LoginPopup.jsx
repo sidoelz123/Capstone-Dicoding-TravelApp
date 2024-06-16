0593fe2e1eae629ba2f2a320d08eb067fa3c1ace
Ajijagatsaputra <ajijagat90@gmail.com>
2024-06-16 17:08:35 +0700

    menambahkan dokumentasi pengguna untuk aplikasi healing go

--- src/components/OrderPopup/LoginPopup.jsx
+++ src/components/OrderPopup/LoginPopup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { IoCloseOutline } from "react-icons/io5";
-import SignupPopup from "./signup.jsx"; // Perbarui path sesuai dengan lokasi file signup.jsx
+import SignupPopup from "./signup.jsx"; 
 
 const LoginPopup = ({ setOpen }) => {
   const [showSignup, setShowSignup] = useState(false);
@@ -8,7 +8,7 @@ const LoginPopup = ({ setOpen }) => {
   return (
     <>
       {showSignup ? (
-        <SignupPopup setOpen={setOpen} /> // Tampilkan SignupPopup jika showSignup true
+        <SignupPopup setOpen={setOpen} /> 
       ) : (
         <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
           <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 rounded-md duration-200 w-[300px]">
